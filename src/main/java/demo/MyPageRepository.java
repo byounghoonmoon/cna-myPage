64448896f770bd80be759c6f04715d246daa5813
byounghoonmoon <passion.mbh@gmail.com>
2020-07-29 13:20:02 +0900

    First Commit

---
+++ src/main/java/demo/MyPageRepository.java
@@ -0,0 +1,10 @@
+package demo;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface MyPageRepository extends CrudRepository<MyPage, Long> {
+
+    List<MyPage> findByOrderId(Long orderId);
+}
